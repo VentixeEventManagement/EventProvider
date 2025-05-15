1026443e55845593c911713290b1f6f3f599ea2f
Martin Mohns <mamoh55@gmail.com>
2025-05-15 03:21:55 +0200

    Added EventFactory

---
+++ EventProvider/EventProvider/Business/Factories/EventFactory.cs
@@ -0,0 +1,37 @@
+﻿using EventProvider.Business.Models;
+using EventProvider.Data.Entities;
+
+namespace EventProvider.Business.Factories
+{
+    public static class EventFactory
+    {
+        public static EventEntity? Create(EventRegistrationModel form) => form == null ? null : new()
+        {
+            Name = form.Name,
+            Description = form.Description,
+            StartDate = form.StartDate,
+            EndDate = form.EndDate,
+            Location = form.Location,
+            TicketPrice = form.TicketPrice,
+            TicketAmount = form.TicketAmount
+        };
+
+        public static Event? Create(EventEntity entity)
+        {
+            if (entity == null)
+                return null;
+
+            return new Event
+            {
+                Id = entity.Id,
+                Name = entity.Name,
+                Description = entity.Description,
+                StartDate = entity.StartDate,
+                EndDate = entity.EndDate,
+                Location = entity.Location,
+                TicketPrice = entity.TicketPrice,
+                TicketsLeft = entity.TicketAmount
+            };
+        }
+    }
+}
--- EventProvider/EventProvider/Business/Models/EventRegistrationModel.cs
+++ EventProvider/EventProvider/Business/Models/EventRegistrationModel.cs
@@ -1,15 +1,14 @@
 ﻿namespace EventProvider.Business.Models
 {
-    public class Event
+    public class EventRegistrationModel
     {
-        public int Id { get; set; } 
+        public int Id { get; set; }
         public string Name { get; set; } = null!;
         public string Description { get; set; } = null!;
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public string Location { get; set; } = null!;
         public int TicketPrice { get; set; }
-        public string TicketsLeft { get; set; } = null!;
-        
+        public string TicketAmount { get; set; } = null!;
     }
 }
